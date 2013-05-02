8acdfa7167a20de91e2491e63fada044f9164f64
Aeryith <aeryith@hotmail.com>
2013-05-02 15:42:55 -0400

    Comment Edits
    Edited comments, added readme file.

---
+++ wow.bat
@@ -0,0 +1,5 @@
+REM Change the beginning directory to where your WoW install is.
+@ECHO OFF
+DEL "C:\Program Files\World of Warcraft\Cache\WDB\enUS\creaturecache.wdb"
+CALL "C:\Program Files\World of Warcraft\Wow.exe"
+EXIT
